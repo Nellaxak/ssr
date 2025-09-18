922039e8a140146a14fc535bac297b18113129ad
Nellaxak <Nellaxak@yandex.ru>
2025-09-18 13:35:27 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+async function CalcData() {
+  let currentDate = new Date()
+  currentDate.setDate(currentDate.getDate());//+1
+  let tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate());//+1
+  //console.log('myDate', currentDate, endNext)
+  let startDate = currentDate.getFullYear() + '-' +
+    (currentDate.getMonth() + 1) + '-' +
+    currentDate.getDate();
+  let endDate = tomorrow.getFullYear() + '-' +
+    (tomorrow.getMonth() + 1) + '-' +
+    tomorrow.getDate();
+  //console.log('return data', startDate, endDate)
+  return new Promise((resolve) => {
+    resolve([startDate, endDate])
+  })
+  //return { startDate, endDate }
+}
+export default async function Home() {
+  let startDate
+  let endDate
+  [startDate, endDate] = await CalcData()
+  //const viewtype = params.viewtype
+  //const items = await http<Item[]>(`http://localhost:3456/${viewtype}`) as Item[];
+  const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
+  //const data = await resp.json()
+  console.log('data', resp.status)
+  return (
+    <ul>
+
+    </ul>
+  )
+}
