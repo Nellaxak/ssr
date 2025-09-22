9a299edf2d374b681aee325cb784e15d7f4a41a5
Nellaxak <Nellaxak@yandex.ru>
2025-09-22 19:50:30 +0300

    readable stream9

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,8 +30,8 @@ export default async function Home() {
   //const viewtype = params.viewtype
   //const items = await http<Item[]>(`http://localhost:3456/${viewtype}`) as Item[];
   const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
-  console.log('date', startDate, endDate)
   const ress=await resp.arrayBuffer()
+  console.log('date', startDate, endDate,ress)
   //let streamResult=new Uint8Array(99999);
   /*let TextResult=''
   await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`)
