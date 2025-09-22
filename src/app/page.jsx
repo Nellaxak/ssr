d99264550e154d18c2790cee015d671e86438da6
Nellaxak <Nellaxak@yandex.ru>
2025-09-22 16:19:07 +0300

    vercel output dir16

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,9 +27,54 @@ export default async function Home() {
   [startDate, endDate] = await CalcData()
   //const viewtype = params.viewtype
   //const items = await http<Item[]>(`http://localhost:3456/${viewtype}`) as Item[];
-  const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
-  //console.log('data', resp.status)
-  if (Number(resp.status) === 200) {
+  //const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
+  console.log('date', startDate, endDate)
+  let streamResult=[]
+  let TextResult=''
+  await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`)
+    .then((response) => response.body)
+    .then((rb) => {
+      const reader = rb.getReader();
+
+      return new ReadableStream({
+        start(controller) {
+          // The following function handles each data chunk
+          function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then(({ done, value }) => {
+              // If there is no more data to read
+              if (done) {
+                console.log("done", done);
+                controller.close();
+                return;
+              }
+              // Get the data and send it to the browser via the controller
+              controller.enqueue(value);
+              // Check chunks by logging to the console
+              console.log('chunk', done, value);
+              const decoder = new TextDecoder('utf-8');
+              // Decode the Uint8Array into a string
+              const textString = decoder.decode(value);
+              //console.log('textString', textString)
+              TextResult=TextResult+textString
+              //streamResult.push(value)
+              push();
+            });
+          }
+
+          push();
+        },
+      });
+    })
+    .then((stream) =>
+      // Respond with our stream
+      new Response(stream, { headers: { "Content-Type": "text/html" } }).text(),
+    )
+    .then((result) => {
+      // Do things with result
+      console.log(result);
+    });
+  /*if (Number(resp.status) === 200) {
     data = await resp.json()
     //console.log('NASA data__________', data)
     const list = data.near_earth_objects
@@ -40,6 +85,6 @@ export default async function Home() {
     ));
   } else {
     console.log('Nasa api request status', resp.status)
-  }
-  return await Li.getList()
+  }*/
+  return TextResult//streamResult//'sssssssssssssssssssssssssss'//await Li.getList()
 }
