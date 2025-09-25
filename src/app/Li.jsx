5e7da9ed21683599cbb112ef650bdf04ca8f9dd0
Nellaxak <Nellaxak@yandex.ru>
2025-09-25 20:41:10 +0300

    regexp59

--- src/app/Li.jsx
+++ src/app/Li.jsx
@@ -80,9 +80,9 @@ class Li {
     return this.status
   }
   async getButton() {
-    const resp = await fetch(`@/app/api/item/${this.id}/status`,
+    /*const resp = await fetch(`@/app/api/item/${this.id}/status`,
       { next: { tags: [`/item/${this.id}/status`] } }
-    )
+    )*/
     //const resp = await fetch(`http://localhost:3000/api/item/${this.id}`
     /*,
      {
@@ -95,7 +95,7 @@ class Li {
     const data = await resp.json()
     //console.log('button data',data)
     //}
-    return createElement('button', { key: 'btn', type: 'submit' }, data.message)
+    return createElement('button', { key: 'btn', type: 'submit' }, 0)//data.message)
   }
   /*async getDate() {
     return createElement('span', { key: 'date' }, this.date)
