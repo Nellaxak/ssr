60139945e959f9979dafa619ea9d8c376323377f
Nellaxak <Nellaxak@yandex.ru>
2025-10-05 18:15:03 +0300

    css flex header toggle13

--- src/app/categories/[viewtype]/layout.jsx
+++ src/app/categories/[viewtype]/layout.jsx
@@ -15,14 +15,12 @@ export default function ParallelLayout({
 }*/) {
   //<SVG />
   //   <div className={styles.wrapLayout}>
-
+  //  <textarea className={styles.h6}>
   return (
     <div className={styles.column}>
-      <header className={styles.header}>
-        <textarea className={styles.h6}>
-          Ближайшие подлёты астероидов
-        </textarea>
-      </header>
+      <textarea className={styles.header}>
+        Ближайшие подлёты астероидов
+      </textarea>
       <nav className={styles.labelWrapper} >
         <Link href="/categories/main"
           className={styles.km}>в километрах</Link>
