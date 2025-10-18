56a99998c09b4e6afb428d0b57a6db2c5c67eed5
Nellaxak <Nellaxak@yandex.ru>
2025-10-18 20:03:03 +0300

    button status api10

--- src/app/actions/updateStatus.js
+++ src/app/actions/updateStatus.js
@@ -1,26 +1,26 @@
 'use server'
 import { revalidateTag } from 'next/cache';
 import { redirect, RedirectType } from 'next/navigation';
-//import Li from '@/app/api/Li';
+import Li from '../Li';
 //import { method } from 'lodash';
 
 export async function buttonClick(formData) {
     //revalidate count
     const id = formData.get('id')
-    /*const item = await Li.findById(Number(id));
+    const item = await Li.findById(Number(id));
     //console.log('find li', id,item)
     const oldStatus = Number(await item.getStatus())
     const newStatus = Number(!Boolean(oldStatus))
-    console.log('fdddd', id, oldStatus, newStatus)*/
-    const resp = await fetch(`api/item/${id}`,{
+    console.log('fdddd', id, oldStatus, newStatus)
+    /*const resp = await fetch(`api/item/${id}`,{
         method:"POST",
         headers: {
             'Content-Type': 'application/json'
         },
         body: id//JSON.stringify(postData)
-    }
+    }*/
      // { next: { tags: [`/item/${this.id}/status`] } }
-    )
+    //)
     //item.status=newStatus
     //revalidateTag(`/item/${id}/status`)
     revalidateTag(`count`)
