601737f922fbdb21149dda0f205d65733f7a843d
Nellaxak <Nellaxak@yandex.ru>
2025-10-18 18:08:00 +0300

    marked page empty list6

--- src/app/marked/page.jsx
+++ src/app/marked/page.jsx
@@ -1,5 +1,5 @@
 //import styles from "./page.module.css";
-import Li from '../../Li'
+import Li from '../Li'
 
 export default function Home() {//async function {params}
   const resd = Li.getList('marked')
