b60943cdcc07b1350ce5c8f346833d4b1e9af5a6
Nellaxak <Nellaxak@yandex.ru>
2025-10-18 12:53:08 +0300

    marked layout1

--- src/app/marked/layout.tsx
+++ src/app/marked/layout.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from "./page.module.css";
 //const inter = Inter({ subsets: ['latin'] })
 
-export function Layout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode,
 }) {
+   //<footer className={styles.footer}>© Все права и планета защищены</footer>
   return (
     <div className={styles.column}>
       <header className={styles.header}>
--- src/app/marked/page.jsx
+++ src/app/marked/page.jsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Li from '../Li'
 
-export default async function Home({ params }) {
+export default async function Home() {
+  //let resd=[]
   const resd = await Li.getList('marked')
   //console.log('resd',resd)
   return resd
