05f122d0b6a3d218999b2fe4052c547f2d7474dd
Nellaxak <Nellaxak@yandex.ru>
2025-10-19 19:07:15 +0300

    update status32

--- src/app/click/[id]/page.jsx
+++ src/app/click/[id]/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Li from '../../../../Li'
+import Li from '../../Li'
 //import ParallelLayout from '../../../../layouts/layout';
 
 
