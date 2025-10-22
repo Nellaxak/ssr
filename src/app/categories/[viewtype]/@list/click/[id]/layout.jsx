1f417a35caac39096a7d08071b7ba7b7141ba718
Nellaxak <Nellaxak@yandex.ru>
2025-10-22 16:19:08 +0300

    update status intercepter route23

--- src/app/categories/[viewtype]/@list/click/[id]/layout.jsx
+++ src/app/categories/[viewtype]/@list/click/[id]/layout.jsx
@@ -6,20 +6,8 @@ import styles from "./page.module.css";
 
 export default async function ParallelLayout({
   children,
-  //count,
-  //list,
 }/*: {
   children: React.ReactNode,
-  count: React.ReactNode,
-  list: React.ReactNode
 }*/) {
-  return (
-    <div className={styles.column}>
-      <header className={styles.header}>
-        <h6 className={styles.h6}>
-          Ближайшие подлёты астероидовrrrrrrrrrrrrrrr
-        </h6>
-      </header>
-    </div>
-  )
+  return 'Ближайшие подлёты астероидовrrrrrrrrrrrrrrr'
 }
