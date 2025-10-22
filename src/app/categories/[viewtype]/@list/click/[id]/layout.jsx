5a5541cc3eb5f6da08edcd7c2a56ce57a1b65a4e
Nellaxak <Nellaxak@yandex.ru>
2025-10-22 16:22:04 +0300

    update status intercepter route24

--- src/app/categories/[viewtype]/@list/click/[id]/layout.jsx
+++ src/app/categories/[viewtype]/@list/click/[id]/layout.jsx
@@ -1,7 +1,7 @@
 //import React from 'React'
 //import SVG from "@/components/SVG/page";
 import Link from "next/link";
-import styles from "./page.module.css";
+//import styles from "./page.module.css";
 //const inter = Inter({ subsets: ['latin'] })
 
 export default async function ParallelLayout({
