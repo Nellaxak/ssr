113e3f7b3ca65f0abf5a156b6ba25b1ea4ede3ca
Nellaxak <Nellaxak@yandex.ru>
2025-10-22 15:49:08 +0300

    update status intercepter route19

--- src/app/categories/[viewtype]/@list/(.)click/[id]/layout.jsx
+++ src/app/categories/[viewtype]/@list/(.)click/[id]/layout.jsx
@@ -13,13 +13,5 @@ export default async function ParallelLayout({
   count: React.ReactNode,
   list: React.ReactNode
 }*/) {
-  return (
-    <div className={styles.column}>
-      <header className={styles.header}>
-        <h6 className={styles.h6}>
-          Ближайшие подлёты астероидовrrrrrrrrrrrrrrr
-        </h6>
-      </header>
-    </div>
-  )
+  return children
 }
