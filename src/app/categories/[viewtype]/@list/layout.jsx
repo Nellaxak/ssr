f94ef874990d9bb481a5197c07e33501e936bb0f
Nellaxak <Nellaxak@yandex.ru>
2025-10-22 16:54:49 +0300

    update status intercepter route28

--- src/app/categories/[viewtype]/@list/layout.jsx
+++ src/app/categories/[viewtype]/@list/layout.jsx
@@ -5,14 +5,10 @@ import styles from "./page.module.css";
 //const inter = Inter({ subsets: ['latin'] })
 
 export default function ParallelLayout({
-  children,
-  //count,
-  //list,
-}/*: {
+  children
+})/*: {
   children: React.ReactNode,
-  count: React.ReactNode,
-  list: React.ReactNode
-}*/) {
+}*/{
   return (
     <ul className={styles.wrapRowRight}>
       {children}
