8facc7ab18675f7df26b8da8a5e93d34a95ca022
Nellaxak <Nellaxak@yandex.ru>
2025-10-22 18:54:24 +0300

    update status20

--- src/app/Li.jsx
+++ src/app/Li.jsx
@@ -112,8 +112,8 @@ class Li {
   }*/
   async setStatus() {
     //console.log('setStatus', value, this.id)
-    let oldStatus=await this.getStatus()
-    this.status = !oldStatus;
+    //let oldStatus=await this.getStatus()
+    this.status = !this.status;
     this.form = [await this.getName(), await this.getButton()]
     this.result = createElement('li', { key: this.id, className: styles.li }, this.form)
     Li.arrResult.push(Number(this.id), this.result)
