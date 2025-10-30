b69b867e2999c695d33036352bd7a3058173c9e3
Nellaxak <Nellaxak@yandex.ru>
2025-10-30 17:23:49 +0300

    update status toggle50

--- src/app/categories/[viewtype]/page.jsx
+++ src/app/categories/[viewtype]/page.jsx
@@ -1,35 +1,41 @@
-import React, { createElement,Suspense} from "react";
+import React, { createElement, Suspense } from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
 import Li from "../../Li";
 //import ParallelLayout from '../../layouts/layout';
 
 let viewtypePromise
-let viewtype1
-let res = 'vvvvvvvvvvvvvvv'
-/*let resM = <nav className={styles.labelWrapper} >
+let viewtype1='main'
+let res = <nav className={styles.labelWrapper} >
+    <Link href="/categories/main"
+        className={styles.km} scroll={false}>в километрах</Link>
+    <span className={styles.space}>|</span>
+    <Link href="/categories/moon"
+        className={styles.moon} scroll={false}>в лунных орбитах</Link>
+</nav>
+let resM = <nav className={styles.labelWrapper} >
     <Link href="/categories/main" scroll={false}
         className={styles.moon}>в километрах</Link>
     <span className={styles.space}>|</span>
     <Link href="/categories/moon" scroll={false}
         className={styles.km}>в лунных орбитах</Link>
-</nav>*/
+</nav>
 export default async function Home({ params }) {
-    //viewtypePromise = await params
-    //viewtype1 = viewtypePromise.viewtype
+    viewtypePromise = await params
+    viewtype1 = viewtypePromise.viewtype
     //console.log('viewtype', viewtype1)
     //if (Li.viewtype !== viewtype1) {
     //Li.initializeData(viewtype1)//await not work
     //Li.viewtype = viewtype1
     //}
-    /*switch (viewtype1) {
+    switch (viewtype1) {
         //case 'main':  
         case 'moon':
-            res //= resM
+            res = resM
             break;
         default: 'default'
-    }*/
-    return null//<Suspense>{res}</Suspense>
+    }
+    return <Suspense>{res}</Suspense>
 }
 /*Home.getLayout = function getLayout(page) {
     return <ParallelLayout>{page}</ParallelLayout>;
