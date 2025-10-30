d1345b20bdb53f230521e68494ee3d7895bee35e
Nellaxak <Nellaxak@yandex.ru>
2025-10-30 13:41:20 +0300

    update status toggle17

--- src/app/categories/[viewtype]/page.jsx
+++ src/app/categories/[viewtype]/page.jsx
@@ -9,9 +9,9 @@ export default async function Home({ params }) {
     console.log('viewtype', viewtype1)
     //if (Li.viewtype !== viewtype) {
     //Li.initializeData(viewtype1)//await not work
-    //Li.viewtype = String(viewtype1)
+    Li.viewtype = String(viewtype1)
     //}
-    Li.viewtype =viewtype1
+    //Li.viewtype =viewtype1
     let res
     if (viewtype1 === 'main') {
         res = <nav className={styles.labelWrapper} >
