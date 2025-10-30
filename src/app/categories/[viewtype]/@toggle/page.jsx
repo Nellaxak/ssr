b9ec1430a8f1cfa25d69fbdb7cc89a38217c2d56
Nellaxak <Nellaxak@yandex.ru>
2025-10-30 19:09:49 +0300

    update status toggle53

--- src/app/categories/[viewtype]/@toggle/page.jsx
+++ src/app/categories/[viewtype]/@toggle/page.jsx
@@ -13,29 +13,30 @@ export default async function Home({ params }) {
     viewtypePromise = await params
     viewtype1 = viewtypePromise.viewtype
     //if (Li.viewtype !== viewtype1) {
-    //await Li.setViewtype(viewtype1)//await not work
+    await Li.setViewtype(viewtype1)//await not work
     //Li.viewtype = viewtype1
     //console.log('sawefv',viewtype1)
     if (viewtype1 === 'main') {
         return (
-            <nav className={styles.labelWrapper} >
-                <Link href="/categories/main" scroll={false}
-                    className={styles.km}>в километрах</Link>
-                <span className={styles.space}>|</span>
-                <Link href="/categories/moon" scroll={false}
-                    className={styles.moon}>в лунных орбитах</Link>
-            </nav>
+            <Suspense>
+                <nav className={styles.labelWrapper} >
+                    <Link href="/categories/main" scroll={false}
+                        className={styles.km}>в километрах</Link>
+                    <span className={styles.space}>|</span>
+                    <Link href="/categories/moon" scroll={false}
+                        className={styles.moon}>в лунных орбитах</Link>
+                </nav></Suspense>
         )
     }
     else {
-        return (
+        return (<Suspense>
             <nav className={styles.labelWrapper} >
                 <Link href="/categories/main" scroll={false}
                     className={styles.moon}>в километрах</Link>
                 <span className={styles.space}>|</span>
                 <Link href="/categories/moon" scroll={false}
                     className={styles.km}>в лунных орбитах</Link>
-            </nav>
+            </nav></Suspense>
         )
     }
     //return <Suspense>{res}</Suspense>
