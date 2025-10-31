346a35306a3f3d7da729f0fc1593548eb0331a79
Nellaxak <Nellaxak@yandex.ru>
2025-10-31 14:40:17 +0300

    first commit12

---
+++ src/app/categories/[viewtype]/@toggle/(.)click/[id]/status/[value]/page.jsx
@@ -0,0 +1,27 @@
+//import Image from "next/image";
+//import styles from "./page.module.css";
+import Li from '../../../../../../../Li'
+//import ParallelLayout from '../../../../layouts/layout';
+
+export default async function Home({ params }) {// 
+  //console.log('sssaaa', process.env)
+  let resd
+  try {
+    const paramsPromise = await params
+    //const viewtype = viewtypePromise.viewtype
+    //console.log('page list item status interceptor', paramsPromise)
+    //const items = await http<Item[]>(`http://localhost:3456/${viewtype}`) as Item[];
+    const find = await Li.findById(paramsPromise.id)
+    //const oldStatus = find.status
+    //get status from url
+    //if (Li.viewtype === viewtype) {
+    await find.setStatus()//paramsPromise.value)
+    //}
+    resd = await Li.getList()//viewtype
+  } catch (err) { console.log('err', err) }
+  //console.log('resd',resd)
+  return resd
+}
+/*Home.getLayout = function getLayout(page) {
+    return <ParallelLayout>{page}</ParallelLayout>;
+};*/
