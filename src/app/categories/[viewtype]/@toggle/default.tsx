d5aeee6174ef15fafeb825dde87d151b6dba1b1f
Nellaxak <Nellaxak@yandex.ru>
2025-10-31 14:54:20 +0300

    first commit15

--- src/app/categories/[viewtype]/@toggle/default.tsx
+++ src/app/categories/[viewtype]/@toggle/default.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function Default() {
-  return null
+  return (<nav className={styles.labelWrapper} >
+    <Link href="/categories/main" scroll={false}
+      className={styles.km}>в километрах</Link>
+    <span className={styles.space}>|</span>
+    <Link href="/categories/moon" scroll={false}
+      className={styles.moon}>в лунных орбитах</Link>
+  </nav>)
 }
