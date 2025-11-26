deb5919697b2174a9c0cc2ba59cb9cdec384cd3a
Nellaxak <Nellaxak@yandex.ru>
2025-11-27 02:54:18 +0300

    css li2

--- src/app/categories/[viewtype]/layout.jsx
+++ src/app/categories/[viewtype]/layout.jsx
@@ -7,15 +7,15 @@ import styles from "./page.module.css";
 //const inter = Inter({ subsets: ['latin'] })
 
 export default async function ParallelLayout({
-  children, //params,
-  list,toggle//count,
+  children, params,
+  list,//toggle//count,
 }/*: {
   children: React.ReactNode,
   count: React.ReactNode,
   list: React.ReactNode
 }*/) {
-  //const paramsPromise = await params
-  //const viewtype = paramsPromise.viewtype
+  const paramsPromise = await params
+  const viewtype = paramsPromise.viewtype
   //console.log('layout viewtype',viewtype)
   /*<main className={styles.column}>
         {(viewtype !== 'marked') ? <header className={styles.header}>
@@ -31,7 +31,16 @@ export default async function ParallelLayout({
   return (
     <Suspense>
       <main className={styles.column}>
-        <Suspense>{toggle}</Suspense>
+        {(viewtype !== 'marked') ? <header className={styles.header}>
+          <h6 className={styles.h6}>Ближайшие подлёты астероидов</h6>
+          <nav  className={styles.labelWrapper}>
+            <Link href="/categories/main" scroll={false}
+              className={(viewtype === 'main') ? 'km' : 'moon'}>в километрах</Link>
+            <span className={styles.space}>|</span>
+            <Link href="/categories/moon" scroll={false}
+              className={(viewtype === 'main') ? 'moon' : 'km'}>в лунных орбитах</Link>
+          </nav></header> :
+          <h6 className={styles.h6}>Заказ отправлен!</h6>}
         <Suspense>{list}</Suspense>
         <Suspense>{children}</Suspense>
       </main>  
