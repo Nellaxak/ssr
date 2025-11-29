9859fbf855a35b89b62cc89c8a0dc52e4e17a41d
Nellaxak <Nellaxak@yandex.ru>
2025-11-29 12:49:21 +0300

    toggleStatus click1

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -127,6 +127,7 @@ async function Row(props) {
 export default async function Home({ searchParams }) {
     [startDate, endDate] = await CalcData()
     const search = await searchParams;
+    console.log('searchParams',search)
     const viewtype = await search.viewtype
     //try {
     resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`,
