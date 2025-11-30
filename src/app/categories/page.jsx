087b30d308dc0a23da88de5213b9a10aa4b1ad62
Nellaxak <Nellaxak@yandex.ru>
2025-11-30 11:53:40 +0300

    toggleStatus click6

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -79,7 +79,7 @@ async function List({ items, renderItem }) {
 async function FormatStatus(params) {
     const status = statusMap.get(params)
     let statusItem = 'ЗАКАЗАТЬ'
-    if (status === 0) {
+    if (status === true) {
         statusItem = 'ЗАКАЗАТЬ'
     }
     else {
@@ -115,7 +115,7 @@ async function Row(props) {
                 <Link key={props.obj.id}
                     className={styles.buttonItem}
                     prefetch={false}
-                    href={`/categories?viewtype=${props.viewtype}&click=${props.obj.id}&status=${!statusMap.get(props.obj.id)}`}
+                    href={`/categories?viewtype=${props.viewtype}&click=${props.obj.id}&status=${statusMap.get(props.obj.id)}`}
                     scroll={false}><Suspense>{String(status)}</Suspense></Link>
                 <span className={styles.danger}>{Danger}</span>
             </div>
