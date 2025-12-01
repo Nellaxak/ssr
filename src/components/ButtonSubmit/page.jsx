18c411c593cdb269fece31ccbad5b7896df434e9
Nellaxak <Nellaxak@yandex.ru>
2025-12-01 20:29:11 +0300

    count2 value13

--- src/components/ButtonSubmit/page.jsx
+++ src/components/ButtonSubmit/page.jsx
@@ -5,16 +5,14 @@ import { useRouter } from 'next/navigation'
 import Form from 'next/form'
 import toggleClick from '../../app/actions/toggleClick'
 //import styles from "./page.module.css";
-
+//closure
 function ButtonSubmit(props) {
-  const handleClick = useCallback((value) => {
-    console.log('click', value)
-    //setViewtype(viewtypeChange)
-  }, [])
-  return <Form action={toggleClick} >
-    <input type='text' name='id' defaultValue={props.id} onChange={() => handleClick('input')} hidden></input>
-    <button type="submit" onClick={() => handleClick(1)}>0</button>
+  return () => {
+    <Form action={toggleClick} >
+    <input type='text' name='id' defaultValue={props.id} hidden></input>
+    <button type="submit">0</button>
   </Form>
+  }
 
 }
-export default ButtonSubmit
+export default ButtonSubmit()
