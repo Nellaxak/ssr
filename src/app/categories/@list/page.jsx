239044a457dcf7aa4610dc20b8fe552649be23d2
Nellaxak <Nellaxak@yandex.ru>
2025-12-01 16:43:02 +0300

    count2 value7

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -154,12 +154,12 @@ export default async function Home({ searchParams }) {
         const list = dat.near_earth_objects
         const arrObjects = Object.values(list)
         if (oldStatus !== undefined) {
-            statusMap.set(id, Number(oldStatus))
-            if (Number(oldStatus) === 0) {
+             if (Number(oldStatus) === 0) {
                 await Item.setCount(-1)
             } else if (Number(oldStatus) === 1) {
                 await Item.setCount(1)
             }
+            statusMap.set(id, Number(oldStatus))//async
             /*statusMap.set(id, Number(!Number(oldStatus)))
             await Item.setCount(Number(!Number(oldStatus)))*/
         }
