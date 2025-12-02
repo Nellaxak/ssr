cebe9f69344f0face6b0eb363d8486c4249beba5
Nellaxak <Nellaxak@yandex.ru>
2025-12-02 16:40:14 +0300

    button renderProp2

--- src/components/ButtonSubmit/page.jsx
+++ src/components/ButtonSubmit/page.jsx
@@ -9,7 +9,7 @@ import toggleClick from '../../app/actions/toggleClick'
 function ButtonSubmit(props) {
   //console.log('ButtonSubmit',props)
   return  <Form action={toggleClick} >
-    <input type='text' name='id' defaultValue={props.id} hidden></input>
+    <input type='number' name='id' defaultValue={props.id} hidden></input>
     <button type="submit">0</button>
   </Form>
 }
