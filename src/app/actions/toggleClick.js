12c2acc57cc2ee7a31912d4dde02bcacff8c0490
Nellaxak <Nellaxak@yandex.ru>
2025-12-02 18:05:25 +0300

    button renderProp3

--- src/app/actions/toggleClick.js
+++ src/app/actions/toggleClick.js
@@ -1,12 +1,10 @@
 'use server'
 import { revalidateTag, revalidatePath } from 'next/cache';
 //import { redirect, RedirectType } from 'next/navigation';
-//import Li from '../Li';
 import statusMap from '../statusMap';
-//import { method } from 'lodash';
 import Item from '../Item';
 async function toggleClick(formData) {
-    console.log('toggleClick', formData, Item.arrObj)
+    console.log('toggleClick', formData, statusMap,Item.arrObj)
     const id = Number(formData.get('id'))
     //console.log('id type',typeof id)
     //const item = Item.arrObj.get(id)//sync
@@ -17,6 +15,5 @@ async function toggleClick(formData) {
     //statusMap.set(id, !oldStatus)
     //revalidatePath('/')
     //revalidateTag('items')//call Item constructor
-    //await Li.setViewtype(formData.get('viewtype'))
 }
 export default toggleClick
