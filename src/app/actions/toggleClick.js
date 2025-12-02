0cf775aac7512c530cf74e8e3e678caa539f6333
Nellaxak <Nellaxak@yandex.ru>
2025-12-02 16:51:10 +0300

    button renderProp3

--- src/app/actions/toggleClick.js
+++ src/app/actions/toggleClick.js
@@ -9,8 +9,8 @@ async function toggleClick(formData) {
     console.log('toggleClick', formData)
     const id = formData.get('id')
     console.log('id type',typeof id)
-    const item = Item.arrObj.get(id)//sync
-    //const item = await Item.findById(id)//sync
+    //const item = Item.arrObj.get(id)//sync
+    const item = await Item.findById(id)//sync
     console.log('item', item)
     //await item.setStatus()//sync
     //console.log('oldStatus',oldStatus)
