8fdd24d4f2d84fc8ebcba350732d25b96e0ff382
Nellaxak <Nellaxak@yandex.ru>
2025-12-02 15:33:53 +0300

    button renderProp2

--- src/app/actions/toggleClick.js
+++ src/app/actions/toggleClick.js
@@ -7,13 +7,13 @@ import { revalidateTag, revalidatePath } from 'next/cache';
 import Item from '../Item';
 async function toggleClick(formData) {
     console.log('toggleClick', formData)
-    const id = Number(formData.get('id'))
-    const item = Item.arrObj.get(id)//sync
-    //const item1 = Item.findById(id)//sync
+    const id = formData.get('id')
+    //const item = Item.arrObj.get(Numberid)//sync
+    const item= Item.findById(Number(id))//sync
     console.log('item', item.id, item)
     item.setStatus()//sync
     //console.log('oldStatus',oldStatus)
-    statusMap.set(id, !oldStatus)
+    statusMap.set(Number(id), !oldStatus)
     //revalidatePath('/')
     revalidateTag('items')
     //await Li.setViewtype(formData.get('viewtype'))
