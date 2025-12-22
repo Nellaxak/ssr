09d26054ebb8bae5e22f18512505dd9b00254e71
Nellaxak <Nellaxak@yandex.ru>
2025-12-22 15:07:22 +0300

    vs fsm46 page increment17

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -92,7 +92,7 @@ async function CalcData(params) {
 }
 
 async function List({ items, renderItem }) {
-    console.log('type items', typeof items)
+    console.log('type items', items.isArray())
     const res = await Promise.all(
         items.map(async (item, index) => {
             //console.log('item_map',item)
