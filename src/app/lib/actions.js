e0613fcf3bc249d4147719aed5f6f1e544cde127
Nellaxak <Nellaxak@yandex.ru>
2025-12-25 18:39:26 +0300

    button status3

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -88,8 +88,9 @@ export async function pagination(index) {
 }
 export async function toggleClick(params) {
     const id = Number(params)
+    console.log('toggle', id, Item)
     const item = await Item.findById(id)
     await item.setStatus()
     //statusMap.set(id, !statusMap.get(id))
-    revalidateTag('items','max')
+    revalidateTag('items', 'max')
 }
