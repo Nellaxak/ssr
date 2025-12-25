1e3ba61d41ee07efb5f149faedd0a6c9c26bb0d7
Nellaxak <Nellaxak@yandex.ru>
2025-12-25 19:08:02 +0300

    button status9

--- src/components/LinkedListInstance/page.tsx
+++ src/components/LinkedListInstance/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { useEffect } from "react";
 import dynamic from 'next/dynamic'
-import { createInstanceLinkedList } from '../../app/lib/actions'
+//import { createInstanceLinkedList } from '../../app/lib/actions'
 
 const LinkedListInstance = () => {
     useEffect(() => {
-        createInstanceLinkedList()
+        //createInstanceLinkedList()
     }, [])
     return <p></p>
 }
