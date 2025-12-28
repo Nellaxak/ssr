2b2a9b129bae9d2db6f4fb8fd1f9d613415333bc
Nellaxak <Nellaxak@yandex.ru>
2025-12-28 11:48:09 +0300

    vs fsm buttonio15+add io3

--- src/components/ButtonSubmit/page.jsx
+++ src/components/ButtonSubmit/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useCallback, Suspense, useRef } from 'react'
+import { useEffect, useCallback, Suspense, useRef, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { toggleClick, mountItem, scrollFSM } from '../../app/lib/actions'
 const options = {
@@ -12,28 +12,36 @@ let ref
 let searchParams
 function ButtonSubmit(props) {
   ref = useRef(null)
+  const [page, setPage] = useState(0);
   //searchParams = useSearchParams()
   const [searchParams, setSearchParams] = useSearchParams();
-  currentPage = Number(searchParams.get('page'))
+
   const handleClick = useCallback(async () => {
     await toggleClick(props.id)
   }, [])
+  useEffect(() => {
+    // This code runs whenever the component mounts or the searchParams change
+    const currentPage = Number(searchParams.get('page'))
+    setPage(currentPage)
+    // You can perform side effects here, such as fetching data based on the new params
+    // fetchData(sort, category);
+  }, [searchParams]);
   //useEffect(() => {
-    // This might capture a stale 'searchParams' if not handled carefully
-    //fetchData(searchParams.get('filter')); 
+  // This might capture a stale 'searchParams' if not handled carefully
+  //fetchData(searchParams.get('filter')); 
   //}, [setSearchParams]);
   //const callbackFunction = useCallback(async (entries: IntersectionObserverEntry[]) => {
   const callbackFunction = useCallback(async (entries) => {
     const [entry] = entries;
     //console.log('currentPage', currentPage)
-    if (currentPage > 0) {
+    if (page > 0) {
       if (entry.isIntersecting) {
         scrollFSM(props.index, 'input')
       } else {
         scrollFSM(props.index, 'output')
       }
     }
-  }, [setSearchParams]);
+  }, [page]);
   useEffect(() => {
     console.log('mount', props.index)
     mountItem(props.index, props.obj)//await
