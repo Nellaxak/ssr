419cd588fd13c5670c2e6dee714e1a9d391143cc
Nellaxak <Nellaxak@yandex.ru>
2025-12-28 11:35:32 +0300

    vs fsm buttonio15+add io3

--- src/components/ButtonSubmit/page.jsx
+++ src/components/ButtonSubmit/page.jsx
@@ -12,8 +12,8 @@ let ref
 let searchParams
 function ButtonSubmit(props) {
   ref = useRef(null)
-  //searchParams = useSearchParams()
-  const [searchParams, setSearchParams] = useSearchParams();
+  searchParams = useSearchParams()
+  //const [searchParams, setSearchParams] = useSearchParams();
   currentPage = Number(searchParams.get('page'))
   const handleClick = useCallback(async () => {
     await toggleClick(props.id)
