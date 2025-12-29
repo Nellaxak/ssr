f12c63744c251fc27f97fdf5c4b377c30499f846
Nellaxak <Nellaxak@yandex.ru>
2025-12-29 12:13:47 +0300

    add io8 ll29

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -186,6 +186,7 @@ export default async function Home({ searchParams }) {
     //console.log('@list Home')
     const search = await searchParams;
     let [startDate, endDate] = await CalcData(search)
+    const viewtype = await search.viewtype
     //try {
     resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`,
         { cache: 'force-cache' },
