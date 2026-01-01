96b3c54acee1ba1691206f4651f97effdd156581
Nellaxak <Nellaxak@yandex.ru>
2026-01-01 21:28:28 +0300

    io data.links3 proxy3

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -192,12 +192,13 @@ const pageProxy = new Proxy(targetPage, {
     },
     set(target, prop, val) {
         if (val !== target[prop]) {
-        //console.log('proxy set ', target, prop, target[prop], val)
-            console.log('added',target, prop, target[prop], val)
+            //console.log('proxy set ', target, prop, target[prop], val)
+            console.log('added', target, prop, target[prop], val)
             target[prop] = val;
             //dll.append(data.links.self)
+            return true
         }
-        return true
+        
     }
 })
 export default async function Home({ searchParams }) {
