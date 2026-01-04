bac74bf68eff7b1b1860665d35148c5a8f82c6ee
Nellaxak <Nellaxak@yandex.ru>
2026-01-04 21:13:10 +0300

    io data.links21

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -113,9 +113,9 @@ async function RenderProp(product, index) {
 }
 async function List({ items, renderItem }) {
     console.log('items', items)
-    for (let element of items) {
+    //for (let element of items) {
         // code to be executed for each element
-    }
+    //}
     /*const res = await Promise.all(items.map(async (item) => {
         //console.log('llpoiyt', item.visible)//linked list
         return await renderItem(item);
