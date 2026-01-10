116f47eca95f472e48a98fefa352438ba8557c62
Nellaxak <Nellaxak@yandex.ru>
2026-01-10 19:43:43 +0300

    scroll component2

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -204,7 +204,7 @@ const pageProxy = new Proxy(targetPage, {
         if (typeof val === 'number') {//once page
             if (val !== target[prop]) {//singleton pattern by proxy
                 target[prop] = val;
-                target.items.push = target.data
+                target.items = target.data
                 //Reflect.set(target,'items',...target.data)
                 //target.items.push(...target.data)
             } else {
