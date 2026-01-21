f8bfaca0f9bd117a147b7a2054634c600dd835b8
Nellaxak <Nellaxak@yandex.ru>
2026-01-21 15:12:11 +0300

    scroll component2

--- src/app/DataLength.js
+++ src/app/DataLength.js
@@ -10,7 +10,7 @@ class DataLength {
   static async getArr(url) {
     //const find = await dll.find(url)
     //console.log('find', find)
-    const arr = await dll.toArray(dll.dataNode.value)
+    const arr = await dll.toArray(dll.dataNode)
     return arr
   }
   static async setArr(pageParam, linkParams, nodeDll) {
