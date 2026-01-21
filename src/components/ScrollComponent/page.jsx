3d7233100804ba428cbeccf8c99254ab77151f24
Nellaxak <Nellaxak@yandex.ru>
2026-01-21 17:49:12 +0300

    scroll component2

--- src/components/ScrollComponent/page.jsx
+++ src/components/ScrollComponent/page.jsx
@@ -91,7 +91,7 @@ function ScrollComponent() {
             //console.log('dataLength', dataLength)
             // Update state, etc.
         })();*/
-        router.push(`?viewtype=${currentViewtype}&page=${page}&scroll=${scroll}`, { scroll: false });
+        router.push(`?viewtype=${currentViewtype}&page=${page}&scroll=${scroll}`, { scroll: true });
         router.refresh()
     }, [startRow, startAction, page, scroll])
     useEffect(() => {
