2d3552f8aba357b5a8d1dad590381b179bae625f
Nellaxak <Nellaxak@yandex.ru>
2026-01-24 13:11:13 +0300

    scroll component3

--- src/components/ScrollComponent/page.jsx
+++ src/components/ScrollComponent/page.jsx
@@ -94,11 +94,11 @@ function ScrollComponent() {
             //console.log('dataLength', dataLength)
             // Update state, etc.
         })();*/
-        router.push(`?viewtype=${currentViewtype}&page=${page}&scroll=${scroll}&col=${countScroll}`, { scroll: true });
+        router.push(`?viewtype=${currentViewtype}&page=${page}`, { scroll: true });
         //router.refresh()
         /*const element = document.querySelector('ol');
         scrollElementToCenter(element);*/
-    }, [startRow, page, scroll, countScroll])
+    }, [page])
     useEffect(() => {
         //find first li , get Height
         //scrollStart(0)
