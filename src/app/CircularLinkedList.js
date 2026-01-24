1bf987090e36393709023870de38fe75e6c07d55
Nellaxak <Nellaxak@yandex.ru>
2026-01-24 14:00:54 +0300

    scroll component3

---
+++ src/app/CircularLinkedList.js
@@ -0,0 +1,55 @@
+export class CircularLinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // Добавление элемента в конец
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            newNode.next = this.head; // Замыкаем на себя
+        } else {
+            let current = this.head;
+            while (current.next !== this.head) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = this.head; // Указываем на голову
+        }
+        this.size++;
+    }
+
+    // Обход списка
+    print() {
+        if (!this.head) return;
+        let current = this.head;
+        do {
+            console.log(current.value);
+            current = current.next;
+        } while (current !== this.head);
+    }
+    async toArray() {
+        const nodes = [];
+        if (!this.head) return;
+        let current = this.head;
+        do {
+            console.log(current.value);
+            current = current.next;
+            nodes.push(currentNode);
+        } while (current !== this.head);
+        /*let currentNode = this.head;
+        while (currentNode) {
+          nodes.push(currentNode);
+          currentNode = currentNode.next;
+        }*/
+
+        return nodes;
+    }
+}
+
+// Использование
+const cll = new CircularLinkedList();
+//cll.print(); // Выведет: 1, 2, 3
+export default cll
