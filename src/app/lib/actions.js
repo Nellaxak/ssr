0c055435e68ffd58221d966bf465a810928abff6
Nellaxak <Nellaxak@yandex.ru>
2026-01-30 17:19:16 +0300

    status change11

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -24,10 +24,7 @@ export async function toggleClick(params) {
     //const item = instanceItem.get(id)
     const item = Item.arrObj.get(id)
     await item.setStatus()
-    //console.log('count', Item.count)
-    //statusMap.set(id, !statusMap.get(id))
-    //revalidateTag('items', 'max')
-    //return item.status
+    revalidateTag('items', 'max')
 }
 export async function getStatus(params) {
     console.log('getStatus', params)
