93a3fdb6781873f7a561b4a5400ac44ddaa98095
Nellaxak <Nellaxak@yandex.ru>
2026-01-30 15:51:18 +0300

    button status change2

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -155,7 +155,7 @@ async function Row(props) {
     /*<span>{props.obj.id}</span>
     <span>{props.key}</span>
     <span>{props.obj.absolute_magnitude_h}</span>*/
-    console.log('qwasxz', props)
+    //console.log('qwasxz', props)
 
     const dataViewtype = props.obj.close_approach_data[0].miss_distance
     //const status = Number(statusMap.get(Number(props.obj.id)))
@@ -210,7 +210,7 @@ export default async function Home({ searchParams }) {
     const scroll = await search.scroll
     //try {
     const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`,
-        { cache: 'force-cache' },
+        /*{ cache: 'force-cache' },*///server actions->Post request
         { next: { tags: ['items'] } }
     );
 
