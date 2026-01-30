c379b6d89704f3c842d05bea9da6f6322dcbe413
Nellaxak <Nellaxak@yandex.ru>
2026-01-30 16:07:13 +0300

    button status change4

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -210,7 +210,7 @@ export default async function Home({ searchParams }) {
     const scroll = await search.scroll
     //try {
     const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`,
-        /*{ cache: 'force-cache' },*///server actions->Post request
+        { cache: 'no-cache'},//server actions->Post request
         { next: { tags: ['items'] } }
     );
 
