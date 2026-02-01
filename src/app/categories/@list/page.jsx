364c6fa333e2ad85dc99191ebc46940326350f52
Nellaxak <Nellaxak@yandex.ru>
2026-02-01 21:06:58 +0300

    status change34

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -116,10 +116,10 @@ async function List({ items, page, scroll, renderItem }) {
     //slice must be 6
     let res
     if (scroll === 'start') {
-        res = await Promise.allSettled(//allSettled
+        res = await Promise.all(//allSettled
             items.slice(page * 10, page * 10 + 12).map(async (item) => {
                 if (item) {
-                    console.log('itemR', item)
+                    //console.log('itemR', item)
                     return await renderItem(item.value);
                 }
             }))
