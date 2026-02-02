5c5fde1d54a7c03e9825d693ddcc66098d10957e
Nellaxak <Nellaxak@yandex.ru>
2026-02-02 16:21:36 +0300

    status change34

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -152,7 +152,9 @@ async function List({ items, page, scroll, renderItem }) {
 async function FormatStatus(params) {
     //console.log('FormatStatus', params)
     //const status = Number(statusMap.get(Number(params)))
-    const status = Item.arrObj.get(Number(params)).status;
+    const find = Item.arrObj.get(Number(params))
+    console.log('find', find)
+    const status = find.status;
     //statusMap.get(Number(params))
     console.log('FormatStatus', params, status)
     let statusItem = 'ЗАКАЗАТЬ'
