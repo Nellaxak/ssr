bf39ce9957692b51b8918f694501f278046389a6
Nellaxak <Nellaxak@yandex.ru>
2026-02-02 18:03:27 +0300

    status change34

--- src/app/categories/@list/page.jsx
+++ src/app/categories/@list/page.jsx
@@ -127,7 +127,7 @@ async function List({ items, page, scroll, renderItem }) {
             items.slice(page * 10, page * 10 + 12).map(async (item) => {
                 if (item) {
                     console.log('itemR', item)
-                    return await renderItem(item);
+                    return await renderItem(item.value);
                 }
             }))
     }
