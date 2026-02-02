e780078539123eec0df4278e9f31182ad2945014
Nellaxak <Nellaxak@yandex.ru>
2026-02-02 18:33:15 +0300

    status change34

--- src/app/worker.js
+++ src/app/worker.js
@@ -2,7 +2,7 @@ import worker_threads from 'node:worker_threads';
 const { Worker, workerData, isMainThread, parentPort } = require('worker_threads');
 //const { parentPort, workerData } = require('worker_threads');
 parentPort.on('message', (msg) => {
-    console.log('in worker', msg)
+    console.log('in_worker', msg)
     const result = msg * 2;
     parentPort.postMessage(result);
 });
